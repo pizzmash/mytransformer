94728951496ed55faba40f35a66e9c6630a34b73
pizzmash <music_xxx_sfy@yahoo.co.jp>
2021-09-09 13:53:57 +0900

    weightは別フォルダに

--- main.py
+++ main.py
@@ -395,7 +395,7 @@ def test(args):
                 )
         if args.need_weights:
             weights_set = mk_weights_set(test_ds.spx, test_ds.spy, data[0].tolist(), data[1].tolist(), decoded, weights.tolist(), data[2].tolist())
-            with open(os.path.join(args.decode_dir, str(i).zfill(len(str(len(test_ds.x)))))+".pickle", mode="wb") as f:
+            with open(os.path.join(args.decode_dir+'-weight', str(i).zfill(len(str(len(test_ds.x)))))+".pickle", mode="wb") as f:
                 pickle.dump(weights_set, f)
         
         decoded_summaries = split_and_decode(decoded, test_ds.spy)
