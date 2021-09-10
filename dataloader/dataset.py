fecdc398b2ecf8552379b8236d8662561580e092
pizzmash <music_xxx_sfy@yahoo.co.jp>
2021-09-10 10:44:40 +0900

    文間の-1を次の文の値に

--- dataloader/dataset.py
+++ dataloader/dataset.py
@@ -51,6 +51,9 @@ class MyDataset(Dataset):
                     for ranks, n_sentences, th in zip(ranks_list, n_sentences_list, ths)]
         else:
           self.z = [torch.tensor(d[2][:max_xlen]) for d in data if len(d[0]) > 0]
+          # 文間の-1を次の文の値にする
+          for i in range(len(self.z)):
+            self.z[i][np.where(self.z[i]==-1)[0]] == np.append(self.z[i], self.z[i].max())[np.where(self.z[i]==-1)[0]+1]
 
     def __getitem__(self, index):
         return self.x[index], self.y[index], self.z[index]
